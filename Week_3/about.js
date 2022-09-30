37de5650360ed5679e52783a13ce57c0e1ce6850
gbull19 <gbull19@yahoo.com>
2022-09-30 08:56:26 -0600

    finished assessment 3

--- Week_3/about.js
+++ Week_3/about.js
@@ -1,14 +1,12 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert("Form Submitted")
 }
+
+document.getElementById("imageContainer").addEventListener("mouseover", (() => alert("You are sexy as a cat")));
 
 
 let form = document.querySelector('#contact');
-
 form.addEventListener('submit', handleSubmit);
---
+++ Week_3/profile.js
@@ -0,0 +1,10 @@
+document.getElementById("color").addEventListener("click", () => {
+    alert("My favorite color is money green")
+});
+
+document.getElementById("place").addEventListener("click", () => {
+    alert("My favorite place is Pier 39 in San Francisco")
+});
+document.getElementById("ritual").addEventListener("click", () => {
+    alert("My favorite ritual is alter sacrifices")
+});
