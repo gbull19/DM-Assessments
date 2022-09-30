1a88697d16dbf61296a54771315de7f9b072b810
gbull19 <gbull19@yahoo.com>
2022-09-30 10:19:46 -0600

    added extra crap to lab

--- Week_3/about.js
+++ Week_3/about.js
@@ -2,11 +2,12 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	alert("Form Submitted")
+	alert("Form Submitted");
+	
 }
 
-document.getElementById("imageContainer").addEventListener("mouseover", (() => alert("You are sexy as a cat")));
-
+document.getElementById("imageContainer").addEventListener("mouseover", (() => 
+	alert("You are sexy as a cat")));
 
 let form = document.querySelector('#contact');
 form.addEventListener('submit', handleSubmit);
--- Week_3/profile.js
+++ Week_3/profile.js
@@ -8,3 +8,20 @@ document.getElementById("place").addEventListener("click", () => {
 document.getElementById("ritual").addEventListener("click", () => {
     alert("My favorite ritual is alter sacrifices")
 });
+
+const revertLebron = () => {
+    document.getElementById("img1").src = "https://github.com/gbull19/hosting/blob/main/the_most_interesting_man_in_the_world_640_03.webp?raw=true";
+}
+
+document.getElementById("img1").addEventListener("click", () => {
+    document.getElementById("img1").src = "https://github.com/gbull19/hosting/blob/main/LeBron%20Flop.jpg?raw=true";
+    setTimeout(revertLebron, 6000);
+});
+
+const revertGengar = () => {
+    document.getElementById("img2").src = "";}
+
+document.getElementById("gengar").addEventListener("click", () => {
+    document.getElementById("img2").src = "https://github.com/gbull19/hosting/blob/main/matheus-carrera-gengar4.jpg?raw=true";
+    setTimeout(revertGengar, 3000);
+})
